c69d9ed4f176e1b1de077b35ef97b0fa44afd889
Dimitar <dimitarivanov2989@gmail.com>
2020-09-05 14:14:00 +0300

    minor improvments

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Route} from 'react-router-dom'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
-const ShopPage = ({collections}) => (
+import CollectionPage from '../collection/collection.component'
+
+//because in app.js shop page is being nested in a route a match property is automatically passed
+const ShopPage = ({match}) => (
     <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,6 @@
 import {createSelector} from 'reselect'
 
+
 const selectShop = state => state.shop
 
 export const selectCollections = createSelector(
@@ -7,3 +8,12 @@ export const selectCollections = createSelector(
     shop=>shop.collections
 )
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key=>collections[key])
+)
+export const selectCollection = collectionUrlParam =>
+    createSelector(
+        [selectCollections],
+        collections => collections[collectionUrlParam]
+    )
