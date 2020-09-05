70f954bbc408822897329f89490f1e28ae7edfec
Dimitar <dimitarivanov2989@gmail.com>
2020-09-05 17:39:31 +0300

    Implemented styled-components library, converted header, homepage and custom button coponents to use it

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
-import './header.styles.scss'
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles.js'
 import { auth } from '../../firebase/firebase.utils'
 
 const Header = () => {
@@ -13,29 +12,29 @@ const Header = () => {
     const { hidden } = useSelector((x) => x.cart);
 
     return (
-        <div className='header'>
-        <Link className='logo-container' to='/'>
+        <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 Shop
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 Contact
-            </Link>
+            </OptionLink>
             {
                 currentUser?.id ? 
-                (<div className='option' onClick={() => auth.signOut()}>Sign Out</div>) :
-                (<Link className='option' to='/signin'>Sign In</Link>)
+                (<OptionLink as='div' onClick={() => auth.signOut()}>Sign Out</OptionLink>) :
+                (<OptionLink to='/signin'>Sign In</OptionLink>)
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {
             hidden ? null : <CartDropdown />
         }
         
-    </div>
+    </HeaderContainer>
     )
     
 }
